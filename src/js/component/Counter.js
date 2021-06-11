0421aa42f54d11ae7e9b0f3290a4c09868722420
Jizga <jizga93@gmail.com>
2021-06-11 12:55:19 +0000

    New architecture about hours, minutes and seconds Counter

--- src/js/component/Counter.js
+++ src/js/component/Counter.js
@@ -1,22 +1,58 @@
-import React from "react";
+import React, { useState } from "react";
 import { ClockElement } from "./ClockElement";
-import { Hours } from "./Hours";
-import { Minutes } from "./Minutes";
-import { Seconds } from "./Seconds";
+import { CounterElement } from "./CounterElement";
 
 export function Counter() {
+	let [counter, setCounter] = useState(0);
+	let [hours, setHours] = useState(0);
+	let [minutes, setMinutes] = useState(0);
+	let [seconds, setSeconds] = useState(0);
+
+	function counterElements() {
+		setInterval(function() {
+			setCounter(counter++);
+			incrementElement("seconds");
+			if (seconds > 59) {
+				setSeconds((seconds = 0));
+				incrementElement("minutes");
+			}
+			if (minutes > 59) {
+				setMinutes((minutes = 0));
+				incrementElement("hours");
+			}
+		}, 1000);
+	}
+
+	function incrementElement(time) {
+		if (time === "hours") {
+			setHours((hours += 1));
+		}
+		if (time === "minutes") {
+			setMinutes((minutes += 1));
+		}
+		if (time === "seconds") {
+			setSeconds((seconds += 1));
+		}
+	}
+
+	window.onload = function() {
+		counterElements();
+	};
+
 	return (
 		<div className="container-fluid d-flex justify-content-center bg-dark p-5 mt-5 myContainer border-top border-bottom border-light">
-			<ClockElement />
-			<Hours />
+			<div className="bg-danger p-5 MyEOpacity border border-light rounded mr-3">
+				<i className="far fa-clock text-white fa-5x"></i>
+			</div>
+			<CounterElement time={hours} />
 			<div className="text-white display-3 mr-1 bg-danger p-5  border border-light rounded mr-3 MyEOpacity">
 				:
 			</div>
-			<Minutes />
+			<CounterElement time={minutes} />
 			<div className="text-white display-3 mr-1 bg-danger p-5  border border-light rounded mr-3 MyEOpacity">
 				.
 			</div>
-			<Seconds seconds={0} />
+			<CounterElement time={seconds} />
 		</div>
 	);
 }
--- src/js/component/CounterElement.js
+++ src/js/component/CounterElement.js
@@ -1,14 +1,16 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-export function Hours() {
+export function CounterElement(props) {
 	return (
 		<div className="d-flex justify-content-start MyEOpacity">
-			<div className="text-white display-3 mr-1 bg-danger p-5  border border-light rounded mr-3">
-				0
-			</div>
 			<div className="text-white display-3 bg-danger p-5 border border-light rounded mr-3">
-				0
+				{props.time}
 			</div>
 		</div>
 	);
 }
+
+CounterElement.propTypes = {
+	time: PropTypes.number
+};
